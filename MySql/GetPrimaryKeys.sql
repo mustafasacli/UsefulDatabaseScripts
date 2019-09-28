470d8ad9f3411fd7f34ddf8ef10efbc675e56144
Mustafa Saçlı <mustasacli@hotmail.com>
2019-09-28 10:03:02 +0300

    Update GetPrimaryKeys.sql
    addition

--- MySql/GetPrimaryKeys.sql
+++ MySql/GetPrimaryKeys.sql
@@ -1,3 +1,7 @@
 https://stackoverflow.com/questions/2341278/how-to-get-primary-key-of-table/2341388
 
 show columns from tablename where `Key` = "PRI";
+
+https://stackoverflow.com/questions/2341278/how-to-get-primary-key-of-table/2341388
+
+SHOW KEYS FROM table WHERE Key_name = 'PRIMARY'
