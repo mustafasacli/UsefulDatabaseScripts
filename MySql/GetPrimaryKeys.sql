34973ab786ed8469c549155f430910881fe6fb49
Mustafa Saçlı <mustasacli@hotmail.com>
2019-09-28 10:39:23 +0300

    Update GetPrimaryKeys.sql
    additional info, spatial data

--- MySql/GetPrimaryKeys.sql
+++ MySql/GetPrimaryKeys.sql
@@ -5,3 +5,7 @@ show columns from tablename where `Key` = "PRI";
 https://stackoverflow.com/questions/2341278/how-to-get-primary-key-of-table/2341388
 
 SHOW KEYS FROM table WHERE Key_name = 'PRIMARY'
+
+** If you have spatial tables in your database, use:
+
+SHOW KEYS FROM table WHERE Key_name = 'PRIMARY' OR Key_name = 'OGR_FID'
