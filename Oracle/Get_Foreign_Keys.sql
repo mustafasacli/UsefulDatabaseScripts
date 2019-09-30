8da1a753af78ceeb8ec4bb0f42612f94fda8d1d0
Mustafa Saçlı <mustasacli@hotmail.com>
2019-09-30 12:51:18 +0300

    Get_Foreign_Keys
    Get_Foreign_Keys

---
+++ Oracle/Get_Foreign_Keys.sql
@@ -0,0 +1,11 @@
+SELECT a.constraint_name, c.owner, a.table_name, a.column_name, 
+       -- referenced pk
+       c.r_owner, c_pk.table_name r_table_name, c_pk.constraint_name r_pk
+  FROM all_cons_columns a
+  JOIN all_constraints c ON a.owner = c.owner
+                        AND a.constraint_name = c.constraint_name
+  JOIN all_constraints c_pk ON c.r_owner = c_pk.owner
+                           AND c.r_constraint_name = c_pk.constraint_name
+ WHERE c.constraint_type = 'R'
+ AND c.owner = :powner 
+ order by C.TABLE_NAME, A.COLUMN_NAME
