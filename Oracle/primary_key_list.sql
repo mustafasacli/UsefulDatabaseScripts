8bc2501190acb57bb0ae3f35084d1a0cf7107837
Mustafa Saçlı <mustasacli@hotmail.com>
2020-01-20 12:03:14 +0300

    Create primary_key_list.sql

---
+++ Oracle/primary_key_list.sql
@@ -0,0 +1,15 @@
+select
+   all_cons_columns.owner as schema_name,
+   all_cons_columns.table_name, 
+   all_cons_columns.column_name, 
+   all_cons_columns.position, 
+   all_constraints.status
+from all_constraints, all_cons_columns 
+where 
+   all_constraints.constraint_type = 'P'
+   and all_constraints.constraint_name = all_cons_columns.constraint_name
+   and all_constraints.owner = all_cons_columns.owner
+order by 
+   all_cons_columns.owner,
+   all_cons_columns.table_name, 
+   all_cons_columns.position
