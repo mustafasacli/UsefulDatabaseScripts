fd79b613f013af5c87b39b20523a97dfa38f1ddf
Mustafa Saçlı <mustasacli@hotmail.com>
2020-01-24 10:12:40 +0300

    Update GetTables.sql

--- PostgreSql/GetTables.sql
+++ PostgreSql/GetTables.sql
@@ -1,1 +1,2 @@
 select * from information_schema.tables where table_catalog = :p_db_name;
+select table_name from information_schema.tables where table_catalog = :p_db_name and table_schema not in ('pg_catalog','information_schema')
