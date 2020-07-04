cac8512ab4e0721ce8bc0ca0460e9a95db7114f6
Mustafa Saçlı <mustasacli@hotmail.com>
2020-07-04 18:28:59 +0300

    Create GetPrimaryKeys.sql

---
+++ PostgreSql/GetPrimaryKeys.sql
@@ -0,0 +1,16 @@
+-- https://dataedo.com/kb/query/postgresql/list-all-primary-keys-and-their-columns
+
+select kcu.table_schema,
+       kcu.table_name,
+       tco.constraint_name,
+       kcu.ordinal_position as position,
+       kcu.column_name as key_column
+from information_schema.table_constraints tco
+join information_schema.key_column_usage kcu 
+     on kcu.constraint_name = tco.constraint_name
+     and kcu.constraint_schema = tco.constraint_schema
+     and kcu.constraint_name = tco.constraint_name
+where tco.constraint_type = 'PRIMARY KEY'
+order by kcu.table_schema,
+         kcu.table_name,
+         position;
